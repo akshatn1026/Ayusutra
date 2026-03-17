936939dc96e555d3e32a236df1e939fe811116ee
nAkshat <akshatmishra880@gmail.com>
2026-03-17 11:50:49 +0530

    new commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   frontendUrl: 'http://localhost:4200',
   appName: 'Ayusutra',
   version: '1.0.0-dev',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://uvhvheclcrgqbaslvsep.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aHZoZWNsY3JncWJhc2x2c2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0ODY5NTgsImV4cCI6MjA4OTA2Mjk1OH0.WB568ZU_QWUsJpaDaE5v7mqUlSyb2ry5OiVL7IUgBjw',
   features: {
     aiAssistant: true,
     videoConsultation: true,
